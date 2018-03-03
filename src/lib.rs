8463c9bd3e86b4d2e1d2afd58bf47a28a95425d4
nel215 <otomo.yuhei@gmail.com>
2018-03-03 14:21:17 +0900

    Implement slow rank

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
 #[cfg(test)]
 mod tests {
+    fn rank(x: i64, i: i32) -> i32 {
+        let mut res = 0;
+        for idx in 0..i {
+            if (x >> idx & 1) > 0 {
+                res += 1;
+            }
+        }
+        return res;
+    }
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(rank(0b10, 1), 0);
+        assert_eq!(rank(0b10, 2), 1);
     }
 }
