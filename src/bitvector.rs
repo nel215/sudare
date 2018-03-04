922de040fcbc6d873e0cd5c5d8a9c092443fb724
nel215 <otomo.yuhei@gmail.com>
2018-03-04 21:57:01 +0900

    Add sum of small block

--- src/bitvector.rs
+++ src/bitvector.rs
@@ -1,22 +1,35 @@
 pub struct BitVector {
     pub large_sum: Vec<u32>,
+    pub small_sum: Vec<u32>,
 }
 pub fn new(n: i32) -> BitVector {
-    let mut size: usize = 0;
+    let mut large_size: usize = 0;
+    let mut small_size: usize = 0;
     if n % 256 > 0 {
-        size += 1;
+        large_size += 1;
     }
-    size += (n / 256) as usize;
-    let large_sum = vec![0; size];
-    return BitVector { large_sum };
+    if n % 32 > 0 {
+        small_size += 1;
+    }
+    large_size += (n / 256) as usize;
+    small_size += (n / 32) as usize;
+    let large_sum = vec![0; large_size];
+    let small_sum = vec![0; small_size];
+    return BitVector {
+        large_sum,
+        small_sum,
+    };
 }
 
 #[test]
 fn check_size() {
     let bv1 = new(1);
     assert_eq!(bv1.large_sum.len(), 1);
+    assert_eq!(bv1.small_sum.len(), 1);
     let bv2 = new(256);
     assert_eq!(bv2.large_sum.len(), 1);
+    assert_eq!(bv2.small_sum.len(), 8);
     let bv3 = new(257);
     assert_eq!(bv3.large_sum.len(), 2);
+    assert_eq!(bv3.small_sum.len(), 9);
 }
